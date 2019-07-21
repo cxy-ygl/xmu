d07c54ec3f6849e3e44e88b11727e3fabe663182
袁林 <1984432352@qq.com>
2019-07-21 15:11:24 +0800

    项目基本结构

---
+++ pages/hotel/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        酒店模块首页文件
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
---
+++ pages/post/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="lygl">
+        <!-- 左边 -->
+        <div class="zuo"></div>
+
+        <!-- 右边 -->
+        <div class="you"></div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+    .lygl{
+        width: 1000px;
+        margin: 0 auto;
+        // background-color: #000;
+        .zuo{
+            width: 265;
+            height: 300px;
+            background-color: #333;
+            margin: 0 0;
+        }
+
+
+        .you{
+            width: 735px;
+            height: 1000px;
+            background-color: #0094ff;
+            margin: -300px 265px;
+        }
+    }
+</style>
+
